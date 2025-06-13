cb9565cac1b7ed2c722629917732cb1309d06dcc
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-06-13 16:41:52 -0700

    first commit

---
+++ components/Test.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const amir = 'this is really good'
+</script>
+
+<template>
+    <h1 class="bg-sky-400">THIS IS Test PAGE</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+    <h1>THIS IS MAIN PAGE</h1>
+    <Test></Test>
+</template>
