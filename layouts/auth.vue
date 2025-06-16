902e072cc64d6a786d6f7a4fd65b2851e5fefe05
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-06-16 16:15:50 -0700

    making auth pages

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -4,16 +4,15 @@ import DefaultLayout from './default.vue';
 
 const { meta } = useRoute();
 const router = useRouter();
-
 </script>
 
 <template>
     <DefaultLayout>
         <div class="w-full h-20 flex items-center bg-gray-900 px-4">
             <div class="flex-1 flex justify-start">
-                <NuxtLink to="/">
+                <button @click="router.back()">
                     <arrow-left-icon class="size-6 text-white" />
-                </NuxtLink>
+                </button>
             </div>
             <div class="flex-1 flex items-center justify-center space-x-2">
                 <img src="~/assets/images/Logo.svg" class="size-6" alt="logo" />
