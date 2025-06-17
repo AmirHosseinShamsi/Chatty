d5a6ba9d32a228fd7ba622ac632472480fd46ef9
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-06-16 19:03:46 -0700

    making register page

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -37,16 +37,52 @@ const inputsData: InputsData[] = [
 </script>
 
 <template>
-    <div class="px-5 space-y-3">
-        <div class="flex flex-col gap-y-1" v-for="item in inputsData">
-            <p class="text-white text-sm capitalize">{{ item.title }}</p>
-            <input
-                :type="item.type"
-                :name="item.name"
-                autocomplete="off"
-                class="py-2 px-3 rounded-lg bg-gray-900 placeholder:text-gray-500 placeholder:text-xs placeholder:capitalize focus:outline-0 text-white text-xs"
-                :placeholder="item.placeholder"
-            />
+    <div class="px-5 flex flex-col justify-between h-full">
+        <div class="space-y-3">
+            <!--profile picture-->
+            <div class="flex flex-col items-center">
+                <!-- Preview (Initially shows a placeholder) -->
+                <div class="relative">
+                    <div
+                        class="w-32 h-32 flex items-center justify-center rounded-full object-cover bg-gray-900"
+                    >
+                        <img
+                            class="size-12"
+                            src="~/assets/images/ProfilePreview.svg"
+                            alt="Profile Preview"
+                        />
+                    </div>
+
+                    <!-- Upload Button Overlay -->
+                    <label
+                        for="avatar-upload"
+                        class="absolute bottom-0 right-0 bg-gray-900 border border-gray-800 p-2 rounded-full"
+                    >
+                        <img
+                            src="~/assets/images/PaperClipIcon.svg"
+                            alt="paper-clip-icon"
+                        />
+                        <input
+                            type="file"
+                            id="avatar-upload"
+                            accept="image/*"
+                            class="hidden"
+                        />
+                    </label>
+                </div>
+            </div>
+            <!-- inputs -->
+            <div class="flex flex-col gap-y-1" v-for="item in inputsData">
+                <p class="text-white text-sm capitalize">{{ item.title }}</p>
+                <input
+                    :type="item.type"
+                    :name="item.name"
+                    autocomplete="off"
+                    class="py-2 px-3 rounded-lg bg-gray-900 placeholder:text-gray-500 placeholder:text-xs placeholder:capitalize focus:outline-0 text-white text-xs"
+                    :placeholder="item.placeholder"
+                />
+            </div>
         </div>
+        <Button title="continue"></Button>
     </div>
 </template>
