5d72b14113c9aa9179f8be9ea2456d04610852ae
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-06-17 14:13:42 -0700

    register/seed is done

--- pages/register/seed.vue
+++ pages/register/seed.vue
@@ -11,19 +11,20 @@ const seedItems = Array.from({ length: 12 });
 </script>
 
 <template>
-    <div class="px-4">
-        <div class="w-full max-w-[300px] rounded-lg bg-gray-900 p-2">
-            <p class="font-Exo2-Regular text-gray-300 text-xs text-justify">
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Pharetra
-                porta neque primis vulputate netus auctor volutpat dignissim.
-                Massa fames aliquam morbi mattis velit arcu. Elementum est
-                mollis elementum velit, neque interdum sed. Quam egestas
-            </p>
-        </div>
-        <div>
+    <div
+        class="px-8 py-10 *:w-full flex flex-col items-center justify-between h-[calc(100vh-80px)]"
+    >
+        <div class="space-y-4">
+            <div class="rounded-lg bg-gray-900 p-2">
+                <p class="font-Exo2-Regular text-gray-300 text-xs text-justify">
+                    Lorem ipsum odor amet, consectetuer adipiscing elit.
+                    Pharetra porta neque primis vulputate netus auctor volutpat
+                    dignissim. Massa fames aliquam morbi mattis velit arcu.
+                    Elementum est mollis elementum velit, neque interdum sed.
+                    Quam egestas
+                </p>
+            </div>
             <h1 class="text-xs text-gray-300 capitalize">your seeds</h1>
-
-            <!--seed items-->
             <div class="grid grid-cols-3 gap-3">
                 <div
                     class="rounded-lg bg-gray-900 text-center py-2 px-4"
@@ -36,8 +37,9 @@ const seedItems = Array.from({ length: 12 });
             <div class="w-full max-w-32 mx-auto">
                 <Button title="regenerate seeds"></Button>
             </div>
-
-            <!--custom checkbox and button-->
+        </div>
+        <!--custom checkbox and button-->
+        <div class="space-y-3">
             <div class="flex items-center gap-x-2 cursor-pointer relative">
                 <input
                     type="checkbox"
