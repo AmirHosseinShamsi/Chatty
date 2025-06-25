c379f25cf782c7705a4814b88883076a0fd04ec9
AmirHossein Shamsi <shamsiamir2000@gmail.com>
2025-06-25 14:33:42 -0700

    fix problem 1

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -2,19 +2,23 @@
 import { ArrowLeftIcon } from '@heroicons/vue/24/solid';
 import DefaultLayout from './default.vue';
 
-const { meta } = useRoute();
+const route = useRoute();
 const router = useRouter();
+
+const pageTitle = computed(() => route.meta.title || 'Default Title');
 </script>
 
 <template>
     <DefaultLayout>
-        <div class="relative w-full h-20 flex items-center justify-center bg-gray-900 px-4">
+        <div
+            class="relative w-full h-20 flex items-center justify-center bg-gray-900 px-4"
+        >
             <button @click="router.back()" class="absolute left-4">
                 <arrow-left-icon class="size-6 text-white" />
             </button>
             <div class="flex items-center space-x-2">
                 <img src="~/assets/images/Logo.svg" class="size-6" alt="logo" />
-                <p class="text-white capitalize text-sm">{{ meta.title }}</p>
+                <p class="text-white capitalize text-sm">{{ pageTitle }}</p>
             </div>
         </div>
         <slot />
